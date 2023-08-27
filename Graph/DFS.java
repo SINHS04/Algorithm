607aad77648e121acb234a353589959e4283a121
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2023-08-27 16:24:33 +0900

    commit

--- Graph/DFS.java
+++ Graph/DFS.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
-public class BFS {
+public class DFS {
 
   static ArrayList<ArrayList<Integer>> vertex; // 연결 리스트
   static int N; // 총 정점 수
@@ -34,19 +32,16 @@ public class BFS {
         .add(Integer.parseInt(str[1]) - 1);
     }
 
-    // 너비 우선 탐색
-    Queue<Integer> list = new LinkedList<Integer>(); // 앞으로 탐색할 정점
-    list.add(0); // 탐색 시작 위치
-    visited[0] = true;
-    System.out.println("Start");
-    while (!list.isEmpty()) {
-      int v = list.poll();
-      System.out.println(v + 1);
-      for (int i : vertex.get(v)) {
-        if (!visited[i]) {
-          visited[i] = true;
-          list.add(i);
-        }
+    dfs(0);
+  }
+
+  // 깊이 우선 탐색
+  public static void dfs(int v) {
+    visited[v] = true;
+    System.out.println(v + 1);
+    for (int i : vertex.get(v)) {
+      if (!visited[i]) {
+        dfs(i);
       }
     }
   }
